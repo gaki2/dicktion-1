96e8ce83a6312d1dc17ecfa2ffb0c90316ebaba7
강수호(sooohka) <shkorea1004@gmail.com>
2022-04-04 21:09:19 +0900

    feat: navbar 완성

--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -1,90 +1,36 @@
 import DOM from "../utils/index.js";
 
-const s = ` <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-<div className="container-fluid">
-  <Link href="/">
-    <a className="navbar-brand">DickTion</a>
-  </Link>
-  <button
-    className="navbar-toggler"
-    type="button"
-    data-bs-toggle="collapse"
-    data-bs-target="#navbarColor01"
-    aria-controls="navbarColor01"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-  >
-    <span className="navbar-toggler-icon"></span>
-  </button>
-
-  <div className="collapse navbar-collapse" id="navbarColor01">
-    <ul className="navbar-nav me-auto">
-      <li className="nav-item">
-        <Link href="#">
-          <a className="nav-link active">
-            Home
-            <span className="visually-hidden">(current)</span>
-          </a>
-        </Link>
-      </li>
-      {/* <li className="nav-item">
-        <a className="nav-link" href="#">
-          Features
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Pricing
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          About
-        </a>
-      </li>
-      <li className="nav-item dropdown">
-        <a
-          className="nav-link dropdown-toggle"
-          data-bs-toggle="dropdown"
-          href="#"
-          role="button"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          Dropdown
-        </a>
-        <div className="dropdown-menu">
-          <a className="dropdown-item" href="#">
-            Action
-          </a>
-          <a className="dropdown-item" href="#">
-            Another action
-          </a>
-          <a className="dropdown-item" href="#">
-            Something else here
-          </a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">
-            Separated link
-          </a>
-        </div>
-      </li> */}
-    </ul>
-  </div>
-</div>
-</nav>`;
-
 export default class Navbar {
-  $El: HTMLElement;
+  private $El!: HTMLElement;
+  private $Content!: HTMLElement;
+  private $Title!: HTMLAnchorElement;
 
   constructor() {
+    this.createEl();
+    this.createContent();
+    this.createTitle("DickTion", "/");
+  }
+
+  private createEl() {
     this.$El = DOM.createEl(
       "nav",
       "navbar navbar-expand-lg navbar-dark bg-primary"
     );
   }
 
+  private createContent() {
+    this.$Content = DOM.createEl("div", "container-fluid");
+  }
+
+  private createTitle(title: string, href: string) {
+    this.$Title = DOM.createEl("a", "navbar-brand") as HTMLAnchorElement;
+    this.$Title.href = href;
+    this.$Title.textContent = title;
+  }
+
   render($parent: HTMLElement) {
+    this.$Content.appendChild(this.$Title);
+    this.$El.appendChild(this.$Content);
     $parent.appendChild(this.$El);
   }
 }
