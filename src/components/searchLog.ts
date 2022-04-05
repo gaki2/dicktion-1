f576d7aa66c9d211076bc83482238d6bd117f8f8
ByeonggakYu <qudrkr0815@korea.ac.kr>
2022-04-05 17:04:10 +0900

    feat: axios 로 받아온 데이터를 파싱하는 로직 추가

--- src/components/searchLog.ts
+++ src/components/searchLog.ts
@@ -10,7 +10,6 @@ export default class SearchLog {
       "div",
       "mt-2 m-auto flex-row pt-3 div_width_50 min_height_span"
     );
-    this.$$buttons = [];
   }
 
   createButton(word: string) {
@@ -21,17 +20,32 @@ export default class SearchLog {
     );
     $button.innerText = word;
     $h4.appendChild($button);
-    this.$$buttons.push($h4);
+    this.$wrapper.appendChild($h4);
   }
 
+  // 컴포넌트를 업데이트함. 기존에 존재하는 엘리먼트를 삭제하고, 새로운 엘리먼트를 생성해서 렌더링함.
+  update(searchLog: string[], $parent: HTMLElement) {
+    this.words = searchLog;
+    this.remove();
+    this.render($parent);
+  }
+
+  // 기존에 존재하는 컴포넌트를 삭제하는 메서드
+  remove() {
+    while (this.$wrapper.hasChildNodes()) {
+      this.$wrapper.removeChild(this.$wrapper.childNodes[0]);
+    }
+  }
+
+  // 컴포넌트를 렌더링하는 메서드
   render($parent: HTMLElement) {
     this.words.forEach((word) => {
       this.createButton(word);
     });
-    for (let i = 0; i < this.$$buttons.length; i += 1) {
-      const $elem = this.$$buttons[i];
-      this.$wrapper.appendChild($elem);
+    // 이렇게 해준 이유는 이 컴포넌트가 리랜더링 될 때, this.$wrapper 가 $parent 에 항상 append 되어있는 상태로 유지하기 위함이다.
+    // 만약 그렇지 않으면 레이아웃이 무너진다 => 서치 로그가 인풋컴포넌트 아래에 위치하게됨
+    if (this.$wrapper.parentNode === null) {
+      $parent.appendChild(this.$wrapper);
     }
-    $parent.appendChild(this.$wrapper);
   }
 }
